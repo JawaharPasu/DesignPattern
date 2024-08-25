2eb226f7a1a7553d2ef13d723583ca3574011355
jawaharPasu <jawaharjw@gmail.com>
2024-08-25 20:40:08 +0530

    added more singleton

--- src/main/java/com/jawa/singleton/BasicSingleton.java
+++ src/main/java/com/jawa/singleton/BasicSingleton.java
@@ -1,12 +1,15 @@
 package com.jawa.singleton;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 /**
  * The type Basic singleton.
  * We are not allowing other classes to create new instances of the class
  * through public constructor. We create the instance inside the class itself
  * and share it when others need it
  */
-public class BasicSingleton {
+public class BasicSingleton implements Serializable {
 
     private int value = 0;
 
@@ -24,5 +27,18 @@ public class BasicSingleton {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    /**
+     * Read resolve object. This is from Serializabale,
+     * as whenever the object is deserialised from file system/ network
+     * it calls this method to return the object to keep the singleton functionality
+     * If the class doesnt implement this, then object no longer exist as singleton
+     *
+     * @return the object
+     */
+    @Serial
+    protected Object readResolve() {
+        return INSTANCE;
     }
 }
